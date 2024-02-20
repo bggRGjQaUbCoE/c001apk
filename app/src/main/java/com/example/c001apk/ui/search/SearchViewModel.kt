18b9d6f7e3b0470151913c057e75211b0767ade0
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-02-20 23:56:21 +0800

    Update SearchViewModel.kt

--- app/src/main/java/com/example/c001apk/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/c001apk/ui/search/SearchViewModel.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.c001apk.logic.database.BlackListDatabase
-import com.example.c001apk.logic.database.SearchHistoryDatabase
-import com.example.c001apk.logic.database.TopicBlackListDatabase
+import com.example.c001apk.logic.database.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class SearchViewModel : ViewModel() {
+class SearchViewModel(private val context: Context) : ViewModel() {
 
     var type: String? = null
     var listSize: Int = -1
@@ -19,35 +17,25 @@ class SearchViewModel : ViewModel() {
     var pageType: String? = null
     var keyWord: String? = null
 
+    private val searchHistoryDao by lazy { SearchHistoryDatabase.getDatabase(context).searchHistoryDao() }
+    private val blackListDao by lazy { BlackListDatabase.getDatabase(context).blackListDao() }
+    private val topicBlackListDao by lazy { TopicBlackListDatabase.getDatabase(context).blackListDao() }
+
     val blackListLiveData: MutableLiveData<List<String>> = MutableLiveData()
-    fun getBlackList(type: String, context: Context) {
-        val newList: MutableList<String> = ArrayList()
+
+    fun getBlackList(blackListType: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            when (type) {
-                "history" -> {
-                    val searchHistoryDao =
-                        SearchHistoryDatabase.getDatabase(context).searchHistoryDao()
-                    newList.addAll(searchHistoryDao.loadAllHistory().map {
-                        it.keyWord
-                    })
-                }
-
-                "userBlacklist" -> {
-                    val blackListDao = BlackListDatabase.getDatabase(context).blackListDao()
-                    newList.addAll(blackListDao.loadAllList().map {
-                        it.keyWord
-                    })
-                }
-
-                "topicBlacklist" -> {
-                    val topicBlacklist = TopicBlackListDatabase.getDatabase(context).blackListDao()
-                    newList.addAll(topicBlacklist.loadAllList().map {
-                        it.keyWord
-                    })
-                }
+            val newList = when (blackListType) {
+                "history" -> getListFromDao(searchHistoryDao)
+                "userBlacklist" -> getListFromDao(blackListDao)
+                "topicBlacklist" -> getListFromDao(topicBlackListDao)
+                else -> emptyList()
             }
             blackListLiveData.postValue(newList)
         }
     }
-
+    
+    private fun getListFromDao(dao: BlackListDao): List<String> {
+        return dao.loadAllList().map { it.keyWord }
+    }
 }
