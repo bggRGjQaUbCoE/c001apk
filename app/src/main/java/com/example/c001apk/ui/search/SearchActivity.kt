ffa8594e2639fc846d6e45270326f4ab9fc81e6b
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-02-21 10:24:55 +0800

    Update SearchActivity.kt

--- app/src/main/java/com/example/c001apk/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/c001apk/ui/search/SearchActivity.kt
@@ -6,24 +6,22 @@ import com.example.c001apk.databinding.ActivitySearchBinding
 import com.example.c001apk.ui.base.BaseActivity
 
 class SearchActivity : BaseActivity<ActivitySearchBinding>() {
-
+    
     private val pageType by lazy { intent.getStringExtra("pageType").orEmpty() }
     private val pageParam by lazy { intent.getStringExtra("pageParam").orEmpty() }
     private val title by lazy { intent.getStringExtra("title").orEmpty() }
-
-    private val searchFragment by lazy {
-        SearchFragment.newInstance(pageType, pageParam, title)
-    }
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupSearchFragment()
-    }
-
-    private fun setupSearchFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.searchFragment, searchFragment)
-            .commit()
+        setContentView(R.layout.activity_search)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.searchFragment,
+                    SearchFragment.newInstance(pageType, pageParam, title)
+                )
+                commit()
+            }
+        }
     }
 }
