de97e352b9602eabb4e592dfa8e0e27c0ee09721
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-04-03 15:43:28 +0800

    fix: fetch topic config

--- app/src/main/java/com/example/c001apk/ui/topic/TopicActivity.kt
+++ app/src/main/java/com/example/c001apk/ui/topic/TopicActivity.kt
@@ -8,6 +8,7 @@ import com.example.c001apk.constant.Constants.LOADING_EMPTY
 import com.example.c001apk.ui.base.BaseViewActivity
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.withCreationCallback
+import java.net.URLDecoder
 
 @AndroidEntryPoint
 class TopicActivity : BaseViewActivity<TopicViewModel>() {
@@ -16,7 +17,7 @@ class TopicActivity : BaseViewActivity<TopicViewModel>() {
         extrasProducer = {
             defaultViewModelCreationExtras.withCreationCallback<TopicViewModel.Factory> { factory ->
                 factory.create(
-                    intent.getStringExtra("url").orEmpty(),
+                    URLDecoder.decode(intent.getStringExtra("url").orEmpty(), "UTF-8"),
                     intent.getStringExtra("title").orEmpty(),
                     intent.getStringExtra("id").orEmpty(),
                     intent.getStringExtra("type").orEmpty(),
