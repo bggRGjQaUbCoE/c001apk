c37a56a734cb5e2899b09225c8440ac794135356
Το μοχθηρό ^_^ <69906215+Malus-risus@users.noreply.github.com>
2024-04-04 14:11:37 +0800

    Optimize

--- GlideImageLoader/src/main/java/net/mikaelzero/mojito/loader/glide/GlideProgressSupport.kt
+++ GlideImageLoader/src/main/java/net/mikaelzero/mojito/loader/glide/GlideProgressSupport.kt
@@ -10,32 +10,25 @@ import java.io.InputStream
 import java.util.*
 
 object GlideProgressSupport {
-    private fun createInterceptor(listener: ResponseProgressListener): Interceptor {
-        return Interceptor { chain: Interceptor.Chain ->
-            val request = chain.request()
-            val response = chain.proceed(request)
-            response.newBuilder()
-                .body(
-                    OkHttpProgressResponseBody(
-                        request.url, response.body,
-                        listener
-                    )
-                )
-                .build()
-        }
-    }
-
     fun init(glide: Glide, okHttpClient: OkHttpClient?) {
-        val builder: OkHttpClient.Builder = okHttpClient?.newBuilder() ?: OkHttpClient.Builder()
-        builder.addNetworkInterceptor(
-            createInterceptor(
-                DispatchingProgressListener()
-            )
-        )
+        val builder = okHttpClient?.newBuilder() ?: OkHttpClient.Builder()
+        builder.addNetworkInterceptor(createInterceptor(DispatchingProgressListener()))
         glide.registry.replace(
             GlideUrl::class.java, InputStream::class.java,
             OkHttpUrlLoader.Factory(builder.build())
         )
+    }
+
+    private fun createInterceptor(listener: ResponseProgressListener): Interceptor {
+        return Interceptor { chain ->
+            val request = chain.request()
+            val response = chain.proceed(request)
+            response.newBuilder()
+                .body(response.body?.let {
+                    OkHttpProgressResponseBody(request.url, it, listener)
+                })
+                .build()
+        }
     }
 
     @JvmStatic
@@ -55,70 +48,48 @@ object GlideProgressSupport {
     }
 
     private interface ResponseProgressListener {
-        fun update(
-            url: HttpUrl,
-            bytesRead: Long,
-            contentLength: Long
-        )
+        fun update(url: HttpUrl, bytesRead: Long, contentLength: Long)
     }
 
     private class DispatchingProgressListener : ResponseProgressListener {
-        override fun update(
-            url: HttpUrl,
-            bytesRead: Long,
-            contentLength: Long
-        ) {
+        companion object {
+            private val LISTENERS: MutableMap<String, ProgressListener?> = HashMap()
+            private val PROGRESSES: MutableMap<String, Int> = HashMap()
+
+            fun forget(url: String) {
+                val key = getRawKey(url)
+                LISTENERS.remove(key)
+                PROGRESSES.remove(key)
+            }
+
+            fun expect(url: String, listener: ProgressListener?) {
+                LISTENERS[getRawKey(url)] = listener
+            }
+
+            private fun getRawKey(formerKey: String): String {
+                return formerKey.split("?")[0]
+            }
+        }
+
+        override fun update(url: HttpUrl, bytesRead: Long, contentLength: Long) {
             val key = getRawKey(url.toString())
-            val listener =
-                LISTENERS[key] ?: return
+            val listener = LISTENERS[key] ?: return
+
             val lastProgress = PROGRESSES[key]
             if (lastProgress == null) {
-                // ensure `onStart` is called before `onProgress` and `onFinish`
                 listener.onDownloadStart()
             }
+
             if (contentLength <= bytesRead) {
                 listener.onDownloadFinish()
                 forget(key)
                 return
             }
+
             val progress = (bytesRead.toFloat() / contentLength * 100).toInt()
             if (lastProgress == null || progress != lastProgress) {
                 PROGRESSES[key] = progress
                 listener.onProgress(progress)
-            }
-        }
-
-        companion object {
-            private val LISTENERS: MutableMap<String, ProgressListener?> =
-                HashMap()
-            private val PROGRESSES: MutableMap<String, Int> =
-                HashMap()
-            private const val URL_QUERY_PARAM_START = "\\?"
-            fun forget(url: String) {
-                LISTENERS.remove(
-                    getRawKey(
-                        url
-                    )
-                )
-                PROGRESSES.remove(
-                    getRawKey(
-                        url
-                    )
-                )
-            }
-
-            fun expect(
-                url: String,
-                listener: ProgressListener?
-            ) {
-                LISTENERS[getRawKey(
-                    url
-                )] = listener
-            }
-
-            private fun getRawKey(formerKey: String): String {
-                return formerKey.split(URL_QUERY_PARAM_START.toRegex())
-                    .toTypedArray()[0]
             }
         }
     }
@@ -127,40 +98,29 @@ object GlideProgressSupport {
         private val mUrl: HttpUrl, private val mResponseBody: ResponseBody?,
         private val mProgressListener: ResponseProgressListener
     ) : ResponseBody() {
-        private var mBufferedSource: BufferedSource? = null
-        override fun contentType(): MediaType? {
-            return mResponseBody!!.contentType()
+        private val mBufferedSource: BufferedSource by lazy {
+            mResponseBody?.source()?.let { source(it).buffer() } ?: throw IllegalStateException("ResponseBody can't be null")
         }
 
-        override fun contentLength(): Long {
-            return mResponseBody!!.contentLength()
-        }
+        override fun contentType(): MediaType? = mResponseBody?.contentType()
 
-        override fun source(): BufferedSource {
-            if (mBufferedSource == null) {
-                mBufferedSource = source(mResponseBody!!.source()).buffer()
-            }
-            return mBufferedSource!!
-        }
+        override fun contentLength(): Long = mResponseBody?.contentLength() ?: -1
+
+        override fun source(): BufferedSource = mBufferedSource
 
         private fun source(source: Source): Source {
             return object : ForwardingSource(source) {
-                private var mTotalBytesRead = 0L
+                private var totalBytesRead = 0L
 
                 @Throws(IOException::class)
                 override fun read(sink: Buffer, byteCount: Long): Long {
                     val bytesRead = super.read(sink, byteCount)
-                    val fullLength = mResponseBody!!.contentLength()
-                    if (bytesRead == -1L) { // this source is exhausted
-                        mTotalBytesRead = fullLength
-                    } else {
-                        mTotalBytesRead += bytesRead
-                    }
-                    mProgressListener.update(mUrl, mTotalBytesRead, fullLength)
+                    totalBytesRead += if (bytesRead != -1L) bytesRead else 0
+
+                    mProgressListener.update(mUrl, totalBytesRead, contentLength())
                     return bytesRead
                 }
             }
         }
-
     }
 }
