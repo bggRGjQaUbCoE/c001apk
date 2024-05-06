88796e59fa834b42d262102e7e5725b4f8a1886b
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-05-06 23:28:48 +0800

    fix: get info from rotated images

--- app/src/main/java/com/example/c001apk/util/ImageUtil.kt
+++ app/src/main/java/com/example/c001apk/util/ImageUtil.kt
@@ -20,6 +20,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
+import androidx.exifinterface.media.ExifInterface
 import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -537,7 +538,16 @@ object ImageUtil {
                     inJustDecodeBounds = true
                 }
                 BitmapFactory.decodeStream(inputStream, null, options)
-                Triple(options.outWidth, options.outHeight, options.outMimeType)
+                var width = options.outWidth
+                var height = options.outHeight
+                with(getRotation(contentResolver, uri)) {
+                    if (this == 90 || this == 270) {
+                        width = height.apply {
+                            height = width
+                        }
+                    }
+                }
+                Triple(width, height, options.outMimeType)
             }
             md5Hash = contentResolver.openInputStream(uri)?.use { inputStream ->
                 calculateMD5(inputStream)
@@ -546,6 +556,29 @@ object ImageUtil {
             e.printStackTrace()
         }
         return Pair(dimensions, md5Hash)
+    }
+
+    private fun getRotation(
+        contentResolver: ContentResolver,
+        uri: Uri
+    ): Int {
+        var rotation = 0
+        try {
+            val exif = contentResolver.openInputStream(uri)?.let { ExifInterface(it) }
+            val orientation = exif?.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_NORMAL
+            )
+            rotation = when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> 90
+                ExifInterface.ORIENTATION_ROTATE_180 -> 180
+                ExifInterface.ORIENTATION_ROTATE_270 -> 270
+                else -> 0
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return rotation
     }
 
     private fun calculateMD5(input: InputStream): ByteArray {
